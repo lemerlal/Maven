8747800c60536c67b633461770158c96b75ed262
lemerlal <43818515+lemerlal@users.noreply.github.com>
2018-11-21 11:09:51 +0100

    Commit : exercice 1

---
+++ TP_ESEO_GIT/src/main/java/com/TP_ESEO_GIT/APP.java
@@ -0,0 +1,10 @@
+package com.TP_ESEO_GIT;
+
+public class APP {
+	public static void main(String [] arg) throws ClassNotFoundException {
+		VilleFranceDAOImpl villeFrance = new VilleFranceDAOImpl();
+		villeFrance.findVilleDAO();
+		
+	}
+
+}
