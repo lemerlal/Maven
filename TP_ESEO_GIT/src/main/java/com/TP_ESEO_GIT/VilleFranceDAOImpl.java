61159712610a7382a75da261d21401c511b94c17
lemerlal <43818515+lemerlal@users.noreply.github.com>
2018-11-28 09:43:26 +0100

    Test deuxiéme buid semi auto

--- TP_ESEO_GIT/src/main/java/com/TP_ESEO_GIT/VilleFranceDAOImpl.java
+++ TP_ESEO_GIT/src/main/java/com/TP_ESEO_GIT/VilleFranceDAOImpl.java
@@ -17,9 +17,7 @@ public class VilleFranceDAOImpl {
 			List  ville = VilleDAO.findVille();
 			
 			for (int i =0 ; i<ville.size(); i++) {
-					System.out.println(((Ville) ville.get(i)).getNom_commune());
 						statement.executeUpdate("INSERT INTO ville_france (Code_commune_INSEE,Nom_commune,Code_postal,Libelle_acheminement,Ligne_5,Latitude,Longitude) VALUES ('"+((Ville) ville.get(i)).getCode_commune()+"','"+((Ville) ville.get(i)).getNom_commune()+"','"+((Ville) ville.get(i)).getCode_postal()+"','"+((Ville) ville.get(i)).getLibelle_acheminement()+"','null','"+((Ville) ville.get(i)).getLatitude()+"','"+((Ville) ville.get(i)).getLongitude()+"')");
-					System.out.println("Test build");
 			}
 			
 			statement.close();
